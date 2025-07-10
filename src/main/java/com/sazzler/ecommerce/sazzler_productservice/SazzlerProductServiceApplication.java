c17d6316ae20be4e738b42a816c91a02a67e48a0
Amodhyh <35001977+amodhyh@users.noreply.github.com>
2025-07-10 16:59:05 +0530

    Initial Commite of the product service

---
+++ src/main/java/com/sazzler/ecommerce/sazzler_productservice/SazzlerProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sazzler.ecommerce.sazzler_productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SazzlerProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SazzlerProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sazzler/ecommerce/sazzler_productservice/SazzlerProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sazzler.ecommerce.sazzler_productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SazzlerProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
